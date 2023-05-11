f91b399bf743be52c4d26456623b32ce9d80153d
ipravdin <pravdin.ihor@gmail.com>
2023-05-11 17:31:45 +0200

    planed structure and added starting files for pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import './App.css';
 import MazeCanvas from "./layouts/components/MazeCanvas";
 
 function App() {
-  return (
-    <div className="App">
-      <MazeCanvas />
-    </div>
-  );
+    // TODO: it will be a starting screen in v2 and v3
+    // additionally in the v3 it will have a loading screen which transitions to starting menu
+    return (
+        <div className="App">
+            {/*TODO: register all routes here*/}
+            {/*<Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+            </Routes>*/}
+            {/*TODO: Add Menu Here*/}
+            <MazeCanvas />
+        </div>
+    );
 }
 
 export default App;
