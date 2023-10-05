bfe3555068cb7254f53a42c39acad50cbdb2f48e
ipravdin <pravdin.ihor@gmail.com>
2023-10-05 14:14:03 +0200

    added playable female character

--- src/components/menu/MenuContent.tsx
+++ src/components/menu/MenuContent.tsx
@@ -8,55 +8,84 @@ import {useDispatch, useSelector} from "react-redux";
 import {gameReset} from "../../store/slices/game-reset";
 import {RootState} from "../../store";
 import {gameplayActions} from "../../store/slices/gameplay";
+import { playerActions } from '../../store/slices/player';
 
-const MenuContent = ({
-    startTitle = false,
-    setMenuState
+export default function MenuContent({
+  startTitle = false,
+  setMenuState
 }: {
-    startTitle?: boolean,
-    setMenuState: Dispatch<SetStateAction<MenuStateType>>
-}) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { firstLaunch } = useSelector((state: RootState) => state.gameplay);
-    const [confirmReset, setConfirmReset] = useState(false);
+  startTitle?: boolean,
+  setMenuState: Dispatch<SetStateAction<MenuStateType>>
+}) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { firstLaunch } = useSelector((state: RootState) => state.gameplay);
+  const { character } = useSelector((state: RootState) => state.player.params);
+  const [confirmReset, setConfirmReset] = useState(false);
 
-    return (
-        <>
-            <MenuView
-                startTitle={startTitle}
-                content={
-                    <>
-                        <Link className="w-full" to={routerLinks.game}>
-                            <button className="btn btn-primary w-full" onClick={() => dispatch(gameplayActions.setFirstLaunchOff())}>
-                                {firstLaunch ? "Start New Game" : "Continue"}
-                            </button>
-                        </Link>
-                        {!firstLaunch && (
-                            <button className="btn" onClick={() => setConfirmReset(true)}>New Game</button>
-                        )}
-                        <button className="btn" onClick={() => setMenuState('options')}>Options</button>
-                        <button className="btn" onClick={() => setMenuState('stats')}>Statistics</button>
-                        <button className="btn" onClick={() => setMenuState('credits')}>Credits</button>
-                    </>
-                }
-            />
-            <Dialog
-                id="confirm-game-reset"
-                open={confirmReset}
-                onClose={() => setConfirmReset(false)}
-                title="Reset a game"
-                content="Are you sure you would like to reset all progress? All statisctics will be lost as well"
-                btnSuccess="No"
-                btnError="Yes"
-                onErrorClick={() => {
-                    // @ts-ignore
-                    dispatch(gameReset());
-                    setTimeout(() => navigate(routerLinks.game), 10);
-                }}
-            />
-        </>
-    );
+  return (
+    <>
+      <MenuView
+        startTitle={startTitle}
+        content={
+          <>
+            {/*@ts-ignore*/}
+            <div className="flex gap-2 items-center justify-center">
+              <label className={`cursor-pointer border-2 box-border ${ character === 'male' ? 'border-white' : 'border-transparent'}`}>
+                <input
+                  className="opacity-0 fixed"
+                  type="radio"
+                  value="male"
+                  name="character"
+                  checked={character === 'male'}
+                  onChange={() => {
+                    dispatch(playerActions.changeCharacter('male'));
+                  }}
+                />
+                <img className="w-28 h-28" src="/player/male/player-b.png" alt="male-character-select" />
+              </label>
+              <label className={`cursor-pointer border-2 box-border ${ character === 'female' ? 'border-white' : 'border-transparent'}`}>
+                <input
+                  className="opacity-0 fixed"
+                  type="radio"
+                  value="female"
+                  name="character"
+                  checked={character === 'female'}
+                  onChange={() => {
+                    dispatch(playerActions.changeCharacter('female'));
+                  }}
+                />
+                <img className="w-28 h-28" src="/player/female/player-b.png" alt="female-character-select" />
+              </label>
+            </div>
+            <Link className="w-full" to={routerLinks.game}>
+              <button className="btn btn-primary w-full" onClick={() => dispatch(gameplayActions.setFirstLaunchOff())}>
+                {firstLaunch ? "Start New Game" : "Continue"}
+              </button>
+            </Link>
+            {!firstLaunch && (
+              <button className="btn" onClick={() => setConfirmReset(true)}>New Game</button>
+            )}
+            <button className="btn" onClick={() => setMenuState('options')}>Options</button>
+            <button className="btn" onClick={() => setMenuState('stats')}>Statistics</button>
+            <button className="btn" onClick={() => setMenuState('credits')}>Credits</button>
+          </>
+        }
+      />
+      <Dialog
+        id="confirm-game-reset"
+        open={confirmReset}
+        onClose={() => setConfirmReset(false)}
+        title="Reset a game"
+        content="Are you sure you would like to reset all progress? All statisctics will be lost as well"
+        btnSuccess="No"
+        btnError="Yes"
+        onErrorClick={() => {
+          // @ts-ignore
+          dispatch(gameReset());
+          setTimeout(() => navigate(routerLinks.game), 10);
+        }}
+      />
+    </>
+  );
 }
-
-export default MenuContent;
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -4,14 +4,23 @@ import {coordToPosition} from "../../utils/helpers";
 import {CoordinateType, OrientationType} from "../../utils/types/maze";
 import {CreatureSizeType, PositionType, SizeType} from "../../utils/types/global";
 import getCreatureSize, {initialCreatureSize} from "./get-creature-size";
-import {PlayerSpriteEnum} from "../../utils/enums/player-sprite";
+
+
+const playerSprite = (character: 'male' | 'female') => ({
+    top: `url('/player/${character}/player-t.png')`,
+    bottom: `url('/player/${character}/player-b.png')`,
+    left: `url('/player/${character}/player-l.png')`,
+    right: `url('/player/${character}/player-r.png')`,
+    dead: `url('/player/${character}/player-dead.png')`
+});
 
 const initialState: PlayerState = {
     params: {
         startCoord: { x: 0, y: 0},
         cellSize: { width: 0, height: 0},
         playerSize: initialCreatureSize,
-        sprite: PlayerSpriteEnum.bottom
+        character: 'male',
+        sprite: playerSprite('male').bottom,
     },
     data: null
 }
@@ -24,10 +33,11 @@ const playerSlice = createSlice({
             const { startCoord, cellSize } = action.payload;
 
             state.params = {
+                ...state.params,
                 startCoord,
                 cellSize,
                 playerSize: getCreatureSize(cellSize),
-                sprite: PlayerSpriteEnum.bottom
+                sprite: playerSprite(state.params.character).bottom,
             };
 
             state.data = new PlayerData(coordToPosition(startCoord, cellSize)).toJson();
@@ -51,16 +61,20 @@ const playerSlice = createSlice({
             if (!state.data) return;
 
             state.data.alive = false;
-            state.params.sprite = PlayerSpriteEnum.dead;
+            state.params.sprite = playerSprite(state.params.character).dead;
         },
         revive(state) {
             if (!state.data) return;
 
             state.data.alive = true;
-            state.params.sprite = PlayerSpriteEnum.bottom;
+            state.params.sprite = playerSprite(state.params.character).bottom;
         },
         changeSprite(state, actions: PayloadAction<OrientationType>) {
-            state.params.sprite = PlayerSpriteEnum[actions.payload];
+            state.params.sprite = playerSprite(state.params.character)[actions.payload];
+        },
+        changeCharacter(state, actions: PayloadAction<'male' | 'female'>) {
+            state.params.character = actions.payload;
+            state.params.sprite = playerSprite(state.params.character).bottom;
         }
     },
 })
@@ -70,7 +84,8 @@ type PlayerState = {
         startCoord: CoordinateType,
         cellSize: SizeType,
         playerSize: CreatureSizeType,
-        sprite: PlayerSpriteEnum
+        sprite: string,
+        character: 'male' | 'female'
     },
     data: PlayerDataJsonType | null
 }
