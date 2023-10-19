e83223319a6eee1dabc417da3f64a2c4e134ec91
ipravdin <pravdin.ihor@gmail.com>
2023-10-19 18:22:04 +0200

    adjusted media screen size p3

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import Game from "./pages/Game";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import routerLinks from "./router-links";
 import TitleScreen from "./pages/TitleScreen";
 import {SoundPlayerProvider} from "./utils/hooks/useSoundPlayer";
 import Menu from './pages/Menu';
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Spinner from './components/Spinner';
+import { Analytics } from '@vercel/analytics/react';
+import { Provider } from 'react-redux';
 
-function App({ className }: { className?: string }) {
-    return (
-        <div className={`text-center w-full h-full ${className ? className : ''}`}>
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={<Spinner />}>
+        <BrowserRouter>
+          <Analytics />
+          <div className="App text-center w-full h-full">
             <SoundPlayerProvider>
-                <Routes>
-                    <Route path={routerLinks.titleScreen} element={<TitleScreen />} />
-                    <Route path={routerLinks.menu} element={<Menu />} />
-                    <Route path={routerLinks.game} element={<Game />} />
-                </Routes>
+              <Routes>
+                <Route path={routerLinks.titleScreen} element={<TitleScreen />} />
+                <Route path={routerLinks.menu} element={<Menu />} />
+                <Route path={routerLinks.game} element={<Game />} />
+              </Routes>
             </SoundPlayerProvider>
-        </div>
-    );
+          </div>
+          <div className="NoDisplay hidden h-full w-full">
+            <div className="flex justify-center items-center text-center p-5">
+              The game is not suitable for small screen sizes, please use another device (i.e. laptop) to access it. Thank you for understanding.
+            </div>
+          </div>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import store, {persistor} from "./store";
-import { PersistGate } from 'redux-persist/integration/react';
-import Spinner from "./components/Spinner";
-import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-    <Provider store={store}>
-        <PersistGate persistor={persistor} loading={<Spinner />}>
-            <BrowserRouter>
-                <Analytics />
-                <App className="App" />
-                <div className="NoDisplay hidden h-full w-full">
-                    <span className="flex justify-center items-center text-center p-5">
-                        The game is not suitable for small screen sizes, please use other device (for instance, laptop) to access it. Thank you for understanding.
-                    </span>
-                </div>
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
