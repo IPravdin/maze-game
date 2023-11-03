7996b6b60be8dc3328ed4b391c24019e840a4b82
ipravdin <pravdin.ihor@gmail.com>
2023-11-03 14:42:07 +0100

    small clean up of unused code

--- src/components/menu/PauseContent.tsx
+++ src/components/menu/PauseContent.tsx
@@ -15,15 +15,7 @@ export default function PauseContent({
       content={
         <>
           <button className='btn btn-primary'>Continue</button>
-          {/*<button className="btn" onClick={() => {
-                            dispatch(mazeActions.generate());
-                            dispatch(gameplayActions.unfroze());
-                        }}>
-                            Restart Level
-                        </button>*/}
           <button type='button' className='btn' onClick={() => setMenuState('options')}>Options</button>
-          {/*<button className="btn" onClick={() => setMenuState('stats')}>Statistics</button>
-                        <button className="btn" onClick={() => setMenuState('credits')}>Credits</button>*/}
           <button className='btn' onClick={() => triggerConfirmLeave(true)}>Leave Game</button>
         </>
       }
