1849fe104c4cfcd8ba6e77f9ecc331839dcc944b
ipravdin <pravdin.ihor@gmail.com>
2024-02-15 15:45:09 +0100

    minor improvements to Enemy.tsx

--- src/components/maze/Enemy.tsx
+++ src/components/maze/Enemy.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { PositionType } from '../../utils/types/global';
 import { CurrMovCoordType, EnemyData } from '../../data/EnemyData';
 import { coordToPosition, objectsEqual, positionToCoord, returnRandomInt } from '../../utils/helpers';
@@ -19,20 +19,14 @@ const Enemy = ({ id, data }: Props) => {
   const { speed, size } = useSelector((state: RootState) => state.enemies.params);
   const { cellSize } = maze.params;
   
-  const [enemy, setEnemy] = useState(new EnemyData({
+  const enemyData = useMemo(() => new EnemyData({
     ...data,
     currentPosition: coordToPosition(data.spawn, cellSize)
-  }));
+  }).toJson(), [data.spawn, data.movement, data.notSpawnRadius, data, cellSize]);
+  
+  const [enemy, setEnemy] = useState(enemyData);
   
   const directionBacklog: number[] = [];
-  
-  // Redefines enemy after component mount to avoid walk-though-walls bug
-  useEffect(() => {
-    setEnemy(new EnemyData({
-      ...data,
-      currentPosition: coordToPosition(data.spawn, cellSize)
-    }));
-  }, [data]);
   
   // For npc movements
   useEffect(() => {
@@ -47,7 +41,7 @@ const Enemy = ({ id, data }: Props) => {
     }
   }, [speed, cellSize]);
   
-  const returnNewPosition = (id: number, { currentPosition, spawn, movement, currMovCoord, sprite }: EnemyData): {
+  const returnNewPosition = (id: number, { currentPosition, spawn, movement, currMovCoord, sprite }: Omit<EnemyData, 'toJson'>): {
     currentPosition: PositionType,
     currMovCoord: CurrMovCoordType,
     sprite: string
