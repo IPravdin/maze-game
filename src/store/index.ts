c9de01786888758af3c3d715298d81fa31a3c671
ipravdin <pravdin.ihor@gmail.com>
2024-02-15 13:54:26 +0100

    fixed redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
 import { Action, combineReducers, configureStore, ThunkAction, ThunkDispatch } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
 import { mazeReducer } from './slices/maze';
 import { playerReducer } from './slices/player';
 import { gameplayReducer } from './slices/gameplay';
 import { enemiesReducer } from './slices/enemies';
 import { statsReducer } from './slices/stats';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
 const persistConfig = {
   key: 'root',
+  version: 1,
   storage,
 };
 
@@ -23,7 +33,13 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
